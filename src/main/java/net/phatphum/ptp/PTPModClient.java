0194dfc81e6120e6239d463186249cfeb37308ca
Phatphum Tarsaku <phumfirebase@gmail.com>
2023-03-31 23:42:01 +0700

    Initial commit

---
+++ src/main/java/net/phatphum/ptp/PTPModClient.java
@@ -0,0 +1,10 @@
+package net.phatphum.ptp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PTPModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/phatphum/ptp/PTPModMain.java
@@ -0,0 +1,16 @@
+package net.phatphum.ptp;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PTPModMain implements ModInitializer {
+	public static final String MODID = "ptp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MODID);
+
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
